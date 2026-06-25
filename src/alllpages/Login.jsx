dcff5765b6ac0a12e801e60b12c619c2e2edf6b1
MonalPingale <pingalemonal5@gmail.com>
2026-06-26 00:04:09 +0530

    Fixed api import path

--- src/alllpages/Login.jsx
+++ src/alllpages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import api from "../api/provider";
 
+import api from "../api/provider"
 export default function Login() {
 
   const navigate = useNavigate();
--- src/components/admin/Analytics.jsx
+++ src/components/admin/Analytics.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import api from "../api/provider";
+import api from "../../api/provider";
 
 import {
   ResponsiveContainer,
